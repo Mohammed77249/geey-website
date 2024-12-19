c5dfb7229df4b5c5ecdbc9b0f5a37874c695a758
Mohammed77249 <mhmdh7550@gmail.com>
2024-12-20 01:51:21 +0300

    no

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -191,7 +191,8 @@
             <span
               class="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center"
             >
-              {{ storeCart.lengthCart }}
+            <p v-if="storeCart.lengthCart!== null">{{ storeCart.lengthCart }} </p>
+             <p v-else>0</p>
             </span>
 
           </div>
